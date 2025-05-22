ba96a903e9ef5e7101e3553f987c188bcf9c8158
hojjatks <hojjatks@gmail.com>
2025-05-21 18:15:33 -0700

    adding the gradients to the Data generation

--- Codes/genLorenz64.py
+++ Codes/genLorenz64.py
@@ -14,6 +14,12 @@ t_eval = np.linspace(*t_span, 50000)
 
 sol = solve_ivp(lorenz63, t_span, [1.0, 1.0, 1.0], t_eval=t_eval)
 X = sol.y.T
-# saving the data to the Data folder
-np.savez('../Data/Lorenz63.npz', X=X)
-print("Data saved to ../Data/Lorenz63.npz")
+# saving the input data to the Data folder
+np.savez('../Data/InputLorenz63.npz', X=X)
+print("Input Data (x) saved to ../Data/InputLorenz63.npz")
+# saving the output data to the Data folder
+dX = np.gradient(X, t_eval, axis=0)
+print("Dimension of the input is", X.shape)
+np.savez('../Data/OutputLorenz63.npz', dX=dX)
+print("Output Data (dx/dt) saved to ../Data/OutputLorenz63.npz")
+print('Dimension of the output is', dX.shape)
